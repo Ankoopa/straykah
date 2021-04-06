b12613389eb8931cc0d0cdffa28222dcc6c73520
Earl Batenga <61070797+Elchupappi@users.noreply.github.com>
2021-04-06 21:34:01 +0800

    ss

---
+++ STRAYKAH/Assets/Scripts/PowerBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class PowerBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image imagePower;
+    [SerializeField]
+    public Text textPower;
+    
+    private bool isDirectionUp = true;
+    public float ammntPower = 0.0f;
+    private float speedPower = 100.0f;
+
+    public GameObject button;
+
+
+    void Start() {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        button.SetActive(true);
+    }
+    void Update()
+    {   
+        PowerActive();
+        if(Input.GetButtonDown("Fire1")){   
+            EndPower();          
+        }
+    }
+    public void PowerActive(){
+        if(isDirectionUp){
+            ammntPower += Time.deltaTime * speedPower;
+            if(ammntPower > 100){
+                isDirectionUp = false;
+                ammntPower = 100.0f;
+            }
+        }
+        else{
+            ammntPower -= Time.deltaTime * speedPower;
+            if (ammntPower < 0){
+                isDirectionUp = true;
+                ammntPower = 0.0f;
+            }
+        }
+        imagePower.fillAmount = (0f - 1f) * ammntPower / 100.0f + 1f;
+        textPower.text = ammntPower.ToString("F0");
+    }
+
+    public void EndPower()
+    {
+        textPower.text = ammntPower.ToString("F0");
+    }
+    
+}
+ 
+
