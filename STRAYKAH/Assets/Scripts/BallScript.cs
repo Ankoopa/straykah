79fceb0c92062867ecc03d3e2570f1c38d094b6a
Ankoopa <dmdasalla@mcm.edu.ph>
2021-04-07 18:14:51 +0800

    added kick force based on power

--- STRAYKAH/Assets/Scripts/BallScript.cs
+++ STRAYKAH/Assets/Scripts/BallScript.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector3 throwDirection;
-    private float kickForce;
-    private Quaternion arrowRotation;
-    private float directionMultiplier;
+    private float defaultKickForce, kickForce;
+    private float directionMultiplier, forceMultiplier;
+    private Text powerText;
 
     public GameObject arrowGuide;
     // Start is called before the first frame update
     void Start()
     {
+        powerText = GameObject.FindGameObjectWithTag("powerLevel").GetComponent<Text>();
         rb = gameObject.GetComponent<Rigidbody>();
-        throwDirection = new Vector3(250, 100, 250);
-        kickForce = 1000f;
-        arrowRotation = arrowGuide.transform.localRotation;
+        throwDirection = new Vector3(150, 50, 150);
+        defaultKickForce = 750f;
     }
 
     // Update is called once per frame
@@ -30,8 +31,6 @@ public class BallScript : MonoBehaviour
         if(Input.GetKey("d")){
             arrowGuide.transform.Rotate(0, 10 * Time.deltaTime, 0);
         }
-
-        print(arrowGuide.transform.localRotation.eulerAngles.y);
         
         if(arrowGuide.transform.localRotation.eulerAngles.y < 320  && arrowGuide.transform.localRotation.eulerAngles.y > 180){
             arrowGuide.transform.eulerAngles = new Vector3(0, -40, 0);
@@ -45,10 +44,11 @@ public class BallScript : MonoBehaviour
         float angle = arrowGuide.transform.localRotation.eulerAngles.y;
         angle = (angle > 180) ? angle - 360 : angle; // converts to negative angle if angled to the left
 
-        print(angle);
-
         directionMultiplier = angle / 40;
         throwDirection.x *= directionMultiplier;
+
+        forceMultiplier = (float)int.Parse(powerText.text)/100;
+        kickForce = defaultKickForce * forceMultiplier;
 
         rb.useGravity = true;
         rb.AddForce(throwDirection.normalized * kickForce);
